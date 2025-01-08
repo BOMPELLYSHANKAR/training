1c133ff7c7be7953b1d5379221f322975d5adb44
BOMPELLYSHANKAR <bompellyshankarrao@gmail.com>
2025-01-08 12:52:34 +0530

    day 3 files

---
+++ functions-ex.js
@@ -0,0 +1,13 @@
+// //without parameters and arguments
+// function demo(){
+//     console.log("hello i am funtion demo !")
+// }
+// demo()
+// with parameters and arguments
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(10,20)
+// add(5,6)
+// add(3,4)
---
+++ looping.js
@@ -0,0 +1,34 @@
+//for-loop
+// document.writeln("javascrpt")
+// for(i=1;i<=5;i++){
+//     console.log("hello world");
+// }
+// even number
+// let n=100;
+// for(i=0;i<n;i++){
+//     if(i%2==0){
+//         console.log(i+" is a even number");
+//     }
+//     else{
+//         console.log(i+" is not a even number");
+//     }
+// }
+//factorial
+// let n=5;
+// let sum=1; 
+// for(let i=n;i>=1;i--){
+//     sum=sum*i;
+// }
+// console.log("factorial of "+n+" is "+sum);
+// while loop
+// let i=1;
+// while(i<=5){
+//     console.log("good morning");
+//     i++;
+// }
+//do-while
+// let i=1;
+// do{
+//     console.log("good morning")
+//     i++;
+// }while(i<=5)
