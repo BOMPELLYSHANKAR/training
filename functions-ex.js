cbf09712ed7fbeb6ac521600205ff46c28a96e33
BOMPELLYSHANKAR <bompellyshankarrao@gmail.com>
2025-01-09 13:16:50 +0530

    day 4 files

--- functions-ex.js
+++ functions-ex.js
@@ -11,3 +11,46 @@
 // add(10,20)
 // add(5,6)
 // add(3,4)
+// function addtocart(){
+//     document.body.style.background='peel';
+//     document.alert()
+// }
+//TYPES OF FUNCTIONS
+//named function example
+// let rev='';
+// function isPalindrome(str){
+//     for(let i=str.length-1;i>=0;i--){
+//         rev+=str[i];
+//     }
+//     if(rev === str){
+//         console.log("it is a palindrome");
+//     }
+//     else{
+//         console.log("it is not a palindrome")
+//     }
+// }
+// isPalindrome("madam");
+// funtion expression
+// let x=function(){
+//     console.log("function expression")
+// }
+// //console.log(x)
+// x()
+// !IIFE
+// (
+//     function(){
+//         console.log("IIFE");
+//     }
+// )
+// ();
+//ARROW FUNTION
+// let x= (a,b) =>console.log(a+b);
+// x(10,5);
+// RETURN KEYWORD--IMPLICIT RETURN,EXPLICIT RETURN
+// let x=(a,b)=> console.log(a+b);
+// x(10,5) //implicit return
+function add(a,b){
+    return a+b;
+}
+result=add(10,5)
+console.log(result);
