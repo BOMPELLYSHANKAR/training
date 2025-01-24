0dde428b796e2e5886f8078cedc6d6296f1fc500
BOMPELLYSHANKAR <bompellyshankarrao@gmail.com>
2025-01-24 10:28:11 +0530

    files added

--- dom.js
+++ dom.js
@@ -1,10 +1,37 @@
-// !document.getElementById("");
+// // !document.getElementById("");
 
-let element = document.getElementById("demo");
-element.innerText = "DOM";
-console.log(element);
+// let element = document.getElementById("demo");
+// element.innerText = "DOM";
+// console.log(element);
 
-let div = document.getElementById("test");
-// div.innerText = "<h1>Header</h1>"
-div.innerHTML = "<h1>Header</h1>"
-console.log(div);
+// let div = document.getElementById("test");
+// // div.innerText = "<h1>Header</h1>"
+// div.innerHTML = "<h1>Header</h1>"
+// console.log(div);
+//!
+// let ele=document.getElementsByClassName("test")
+// //console.log(ele)
+// //console.log(Array.isArray(ele))
+// //spread operator:it is used to takeout each and every value from original array and create a new array
+// let x=[...ele]
+// console.log(x);
+// console.log(Array.isArray(x))
+// x.map((Element)=>{
+//     //console.log(element)
+//     Element.style.backgroundColor='teal'
+// })
+// let ele=document.getElementsByTagName("div")
+// console.log(ele)
+// let ele=document.getElementsByName("username")
+// console.log(ele)
+// let ele=document.querySelector("#demo")
+// console.log(ele)
+// let ele=document.querySelectorAll(".test");
+// // console.log(ele)
+// [...ele].map(element=>{
+//     console.log(element.innerText)
+// })
+// let btn=document.getElementById("btn")
+// btn.addEventListener("click",()=>{
+//     console.log("button clicked")
+// })
