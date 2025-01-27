38233658bd17151552807b53f9ff625f18293d36
BOMPELLYSHANKAR <bompellyshankarrao@gmail.com>
2025-01-27 20:31:15 +0530

    added files

---
+++ dom1.js
@@ -0,0 +1,42 @@
+// let x=document.createElement("h1")
+// x.innerText="Dynamic cretaion of html element from js";
+// //x.id="demo"
+// x.setAttribute("id",demo)
+// console.log(X)
+// document.body.appendChild(x)
+// let image=document.createElement("img")
+// image.src="./360628.jpg"
+// console.log(image);
+// document.body.appendChild(image)
+// dynamic elements
+// let mainEle=document.createElement("div");
+// mainEle.setAttribute("class","mainblock");
+// // mainEle.innerText="main";
+// console.log(mainEle)
+// let topEle=document.createElement("div")
+// topEle.setAttribute("class","topblock")
+// // topEle.innerText="top"
+// // console.log(topEle)
+// let image=document.createElement("img")
+// image.src="https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_1280.jpg";
+// image.width="300"
+// image.height="300"
+
+// let bottomEle=document.createElement("div")
+// bottomEle.setAttribute("class","bottomblock")
+// // topEle.innerText="bottom"
+// //console.log(bottomEle)
+// let h1=document.createElement("h1");
+// h1.innerText="Heading"
+// // console.log(h1)
+// let btn=document.createElement("button")
+// btn.innerText="view more"
+// //console.log(btn)
+// topEle.appendChild(image)
+// bottomEle.appendChild(h1)
+// bottomEle.appendChild(btn)
+// mainEle.appendChild(topEle)
+// mainEle.appendChild(bottomEle)
+// document.body.appendChild(mainEle)
+
+
